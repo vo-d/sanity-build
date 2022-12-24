ea5da8b3cfd178b7f45ff32a69b67c0bef2891a9
vo-d <daidaivo2002@gmail.com>
2022-12-24 16:44:51 -0600

    banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div className=' max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,7 +13,13 @@ const Home: NextPage = () => {
 
       <Header />
 
-      
+      <div className=' flex justify-between items-center bg-yellow-400 border-y border-black'>
+        <div className=' p-10 space-y-5'>
+          <h1 className='text-6xl max-w-xl font-serif'><span className=' underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect</h1>
+          <h2>It's easy and free to poast your thinking on any topic and connect with millions of readers.</h2>
+        </div>
+        <img className=' hidden md:inline-flex h-32 lg:h-full' src="pngwing.com.png" alt="" />
+      </div>
     </div>
   )
 }

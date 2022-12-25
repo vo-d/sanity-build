0a8a00ca948f842ce3aba0ae88b85f6a9836bf9a
vo-d <daidaivo2002@gmail.com>
2022-12-24 22:58:37 -0600

    fetch from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
 
-const Home: NextPage = () => {
+import Head from 'next/head'
+import {Post} from '../typings'
+import Header from '../components/Header'
+import {sanityClient, urlFor} from "../sanity.js"
+
+interface Props{
+  posts: [Post]
+}
+
+export default function Home({posts} : Props) {
+  console.log(posts)
   return (
     <div className=' max-w-7xl mx-auto'>
       <Head>
@@ -24,4 +30,25 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+
+
+export const getServerSideProps = async() =>{
+  const query = `*[_type == "post"]{
+    _id,
+      title,
+      slug,
+      author ->{
+        name, image,
+      },
+      description,
+      mainImage,
+      slug
+  }`;
+
+  const posts =  await sanityClient.fetch(query)
+  return{
+    props:{
+      posts
+    }
+  }
+}
---
+++ sanity.js
@@ -0,0 +1,17 @@
+import {createImageUrlBuilder, createClient} from 'next-sanity'
+
+export const config={
+ dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production" ,
+ projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+ apiVersion:"2022-03-25",
+
+ /* set usdCdn to false if your app require the freshest possible data always (potentially slightly slower and a bit more expensive).
+ authenticated request(like preview) will always bypass the CDN */
+ useCdn: process.env.NODE_ENV === 'production',
+};
+
+//set up the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config)
+
+// set up  a helper function for generating image URLs with only the asset reference data in your documents
+export const urlFor = (source)=> createImageUrlBuilder(config).image(source)

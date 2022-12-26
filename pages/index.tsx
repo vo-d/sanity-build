2b8069df1c386b08db01b77d2602c20169b1ec08
vo-d <daidaivo2002@gmail.com>
2022-12-25 19:35:12 -0600

    fetch post info & style

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,6 @@ interface Props{
 }
 
 export default function Home({posts} : Props) {
-  console.log(posts)
   return (
     <div className=' max-w-7xl mx-auto'>
       <Head>
@@ -29,10 +28,19 @@ export default function Home({posts} : Props) {
         <img className=' hidden md:inline-flex h-32 lg:h-full' src="pngwing.com.png" alt="" />
       </div>
 
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map((post)=> (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <img src={urlFor(post.mainImage).url()} alt="" />
+            <div className='group cursor-pointer border rounded-lg overflow-hidden'>
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200' src={urlFor(post.mainImage).url()} alt="" />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12  rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+              </div>
+            </div>
           </Link>
         ))}
       </div>
